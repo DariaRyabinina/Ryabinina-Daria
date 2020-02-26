393bda3834c61f5092c08c88c67dd01f9aecb389
Daria <ryabinina@i-teco.ru>
2020-02-26 09:19:47 +0300

    Урок 6

---
+++ Ryabinina-D/src/Lesson06/Less06Task3.java
@@ -0,0 +1,50 @@
+package Lesson06;
+
+/*public boolean isUnique(Map<String, String> map);
+Написать метод, который возвращает true, если в мапе нет двух и более
+одинаковых value, и false в противном случае. Для пустой мапы метод
+возвращает true.
+Например, для метода {Вася=Иванов, Петр=Петров, Виктор=Сидоров,
+Сергей=Савельев, Вадим=Викторов} метод вернет true, а для {Вася=Иванов,
+Петр=Петров, Виктор=Иванов, Сергей=Савельев, Вадим=Петров} метод
+вернет false.
+
+ */
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Less06Task3 {
+    public static boolean isUnique(Map<String, String> map) {
+        for (String o : map.keySet()) {
+            for (String o1 : map.keySet()) {
+                if (map.get(o).equals(map.get(o1))) {
+                    if (!o.equals(o1)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Map<String, String> staf = new HashMap<>();
+
+
+        staf.put("Вася", "Иванов");
+        staf.put("Коля", "Петров");
+        staf.put("Саша ", "Иванов");
+        for (String s : staf.keySet()) {
+            System.out.println(s + " " + staf.get(s));
+        }
+        isUnique(staf);
+        if (isUnique(staf)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
