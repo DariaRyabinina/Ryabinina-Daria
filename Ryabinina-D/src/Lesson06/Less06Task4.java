0257fd8d4244bb8e762eb5c8c8e28fbc30f26603
Daria <ryabinina@i-teco.ru>
2020-03-03 17:48:37 +0300

    Урок 6

--- Ryabinina-D/src/Lesson06/Less06Task4.java
+++ Ryabinina-D/src/Lesson06/Less06Task4.java
@@ -5,35 +5,36 @@ package Lesson06;
 Например, для множества ["foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude"]
 метод вернет ["foo", "bar", "spoon", "!"]
  */
+
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
 public class Less06Task4 {
-        public static Set<String> removeEvenLength(Set<String> set) {
-            Iterator<String> iterator = set.iterator();
-            while (iterator.hasNext()) {
-                if (iterator.next().length() % 2 == 0) {
+    public static Set<String> removeEvenLength(Set<String> set) {
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().length() % 2 == 0) {
 
-                    iterator.remove();
-                }
+                iterator.remove();
             }
-            return set;
         }
-
-        public static void main(String[] args) {
-            Set<String> set = new HashSet<String>();
-            set.add("foo");
-            set.add("buzz");
-            set.add("bar");
-            set.add("fork");
-            set.add("bort");
-            set.add("spoon");
-            set.add("!");
-            set.add("dude");
-            System.out.println(set);
-            System.out.println(removeEvenLength(set));
-        }
-
+        return set;
     }
+
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+        set.add("foo");
+        set.add("buzz");
+        set.add("bar");
+        set.add("fork");
+        set.add("bort");
+        set.add("spoon");
+        set.add("!");
+        set.add("dude");
+        System.out.println(set);
+        System.out.println(removeEvenLength(set));
+    }
+
+}
 
